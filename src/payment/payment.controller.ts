5adc03836dcd29f0fa8bea19f9935dd4c1e4b8fe
hamza-yameen <hamzayameen3000@gmail.com>
2024-09-08 16:38:14 +0500

    add authorization in payment module

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,18 +1,21 @@
-import { Controller, Post, Body, Param, Get } from '@nestjs/common';
-import { PaymentService } from './payment.service';
-import { CreatePaymentDto } from './dto';
+import { Controller, Post, Body, Param, Get } from "@nestjs/common";
+import { PaymentService } from "./payment.service";
+import { CreatePaymentDto } from "./dto";
+import { Authorized } from "libs/decorators";
 
-@Controller('payment')
+@Controller("payment")
 export class PaymentController {
-  constructor(private readonly paymentService: PaymentService) {}
+	constructor(private readonly paymentService: PaymentService) {}
 
-  @Post('/scan')
-  create(@Body() createPaymentDto: CreatePaymentDto) {
-    return this.paymentService.create(createPaymentDto);
-  }
+	@Authorized()
+	@Post("/scan")
+	create(@Body() createPaymentDto: CreatePaymentDto) {
+		return this.paymentService.create(createPaymentDto);
+	}
 
-  @Get(':bookingId')
-  getPaymentDetailByBookingId(@Param('bookingId') bookingId: string) {
-    return this.paymentService.getPaymentDetailByBookingId(+bookingId);
-  }
+	@Authorized()
+	@Get(":bookingId")
+	getPaymentDetailByBookingId(@Param("bookingId") bookingId: string) {
+		return this.paymentService.getPaymentDetailByBookingId(+bookingId);
+	}
 }
--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { PaymentService } from './payment.service';
-import { PaymentController } from './payment.controller';
-import { PrismaService } from 'src/prisma.service';
+import { Module } from "@nestjs/common";
+import { PaymentService } from "./payment.service";
+import { PaymentController } from "./payment.controller";
+import { PrismaService } from "src/prisma.service";
+import { AuthModule } from "libs/modules/auth.module";
+import { TokenService } from "libs/modules/auth.module";
 
 @Module({
-  controllers: [PaymentController],
-  providers: [PaymentService, PrismaService],
+	imports: [AuthModule],
+	controllers: [PaymentController],
+	providers: [PaymentService, PrismaService, TokenService],
 })
 export class PaymentModule {}
